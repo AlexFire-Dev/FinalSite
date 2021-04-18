ee54f3368bb2fd0f27aabe8936a3596c56e1075d
Alex Shvarev <alex@shvarev.com>
2021-04-18 22:41:41 +0300

    v0.15 Design improvements

--- apps/notes/forms.py
+++ apps/notes/forms.py
@@ -7,8 +7,3 @@ class CreateNoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ('title', 'text', 'status')
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'text': forms.Textarea(attrs={'class': 'form-control'}),
-            'status': forms.CheckboxInput(attrs={'class': 'form-check'}),
-        }
