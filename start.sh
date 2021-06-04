3c3fe9853b403690c6f1c5ec389634a4ae227590
AlexFire-Dev <74737162+AlexFire-Dev@users.noreply.github.com>
2021-06-04 22:15:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 manage.py collectstatic
+python3 manage.py collectstatic --clear
 python3 manage.py migrate
 
 gunicorn FinalSite.wsgi:application -w 4 -t 600 -b 0.0.0.0:8000
